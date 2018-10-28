d5d8d9c802a2ed40f1c17d4e4b23caff36cf6a1d
N Babington <nicholas.babington@hotmail.com>
2018-10-28 18:01:53 -0400

    instead of church

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+var random;
+var lose;
+var win;
+
+$(document).ready(function () {
+   
+    var random = Math.floor(Math.random() * 100) 
+    console.log(randomNum)
+    
+   
+    var yellowNumber = Math.floor(Math.random() * 12) 
+    console.log(blueNum)
+    var blueNumber = Math.floor(Math.random() * 12) 
+    console.log(pinkNum)
+    var redNumber = Math.floor(Math.random() * 12) 
+    console.log(greenNum)
+    var pinkNumber = Math.floor(Math.random() * 12)
+    console.log(yellowNum)
+
+}
