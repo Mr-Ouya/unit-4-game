771e25e8b275c3b5c4972ee5d73ca5331c17ace0
N Babington <nicholas.babington@hotmail.com>
2018-10-29 06:51:48 -0400

    lossesCounternotworking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,99 @@
-
-var random;
-var lose;
-var win;
-
 $(document).ready(function () {
    
-    var random = Math.floor(Math.random()) 
-    console.log(randomNum)
-    
-   
-    var yellowNumber = Math.floor(Math.random()) 
-    console.log(blueNum)
-    var blueNumber = Math.floor(Math.random()) 
-    console.log(pinkNum)
-    var redNumber = Math.floor(Math.random()) 
-    console.log(greenNum)
-    var pinkNumber = Math.floor(Math.random()g)
-    console.log(yellowNum)
+    var playerScore = 0;
+    var losses = 0;
+    var wins = 0;
+//Make and display random number
 
-}
+    var randomNum = Math.floor(Math.random() * 100) + 19
+    $("#randNum").html(randomNum)
+   
+    var blueNum = Math.floor(Math.random() * 10) + 1
+    var pinkNum = Math.floor(Math.random() * 10) + 1
+    var greenNum = Math.floor(Math.random() * 10) + 1
+    var yellowNum = Math.floor(Math.random() * 10) + 1
+
+
+
+    var reset = function() {
+        randomNum = Math.floor(Math.random() * 100) + 19
+        $("#randNum").html(randomNum)
+       
+        blueNum = Math.floor(Math.random() * 10) + 1
+        pinkNum = Math.floor(Math.random() * 10) + 1
+        greenNum = Math.floor(Math.random() * 10) + 1
+        yellowNum = Math.floor(Math.random() * 10) + 1
+        
+    
+        playerScore = 0;
+        $("#score").html(playerScore)
+    }
+
+    $("#score").html(playerScore)
+    $("#wins").html(wins)
+    $("#losses").html(losses)
+
+
+    $("#blueCrystal").on("click", function () {
+        playerScore += parseInt(blueNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            wins++
+            $("#winsCounter").html(wins)
+            reset()
+
+        }
+        if (playerScore > randomNum) {
+            losses++
+            $("#lossesCounter").html(losses)
+            reset()
+        }
+    })
+
+    $("#pinkCrystal").on("click", function () {
+        playerScore += parseInt(pinkNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            wins++
+            $("#winsCounter").html(wins)
+            reset()
+        }
+        if (playerScore > randomNum) {
+            losses++
+            $("#lossesCounter").html(losses)
+            reset()
+        }
+    })
+
+    $("#redCrystal").on("click", function () {
+        playerScore += parseInt(greenNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            wins++
+            $("#winsCounter").html(wins)
+            reset()
+
+        }
+        if (playerScore > randomNum) {
+            losses++
+            $("#lossesCounter").html(losses)
+            reset()
+        }
+    })
+
+    $("#yellowCrystal").on("click", function () {
+        playerScore += parseInt(yellowNum)
+        $("#score").html(playerScore)
+        if (playerScore === randomNum) {
+            wins++
+            $("#winsCounter").html(wins)
+            reset()
+        }
+        if (playerScore > randomNum) {
+            losses++
+            $("#lossesCounter").html(losses)
+            reset()
+        }
+    })
+
+})
